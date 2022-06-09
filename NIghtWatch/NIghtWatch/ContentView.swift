8b61727386d9b62c7af1d55d2e73d5cca2424c43
Gustavo Salazar <gustavo.salazar@accenture.com>
2022-06-09 11:22:46 -0500

    Building SIngle View Applications
    Se usan el Group,HStack,VStack,Text

--- NIghtWatch/NIghtWatch/ContentView.swift
+++ NIghtWatch/NIghtWatch/ContentView.swift
@@ -13,31 +13,75 @@ struct ContentView: View {
         VStack{
             HStack {
                 VStack(alignment: .leading){
-                    Text("Nightly Tasks")
-                        .underline()
-                        .font(.title3)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.blue)
-                        .textCase(.uppercase)
-                    Text("Check all windows")
-                    Text("Check all doors")
-                    Text("Check that the safe is locked")
-                    Text("Check the mailbox")
-                    Text("Inspect security Camares")
-                    Text("Clear ice from sidewalks")
-                    Text("Document \"strange and unusual\" ocurrenses")
-                    Text("Weekly Tasks")
-                        .underline()
-                        .font(.title3)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.blue)
-                        .textCase(.uppercase)
-                    Text("Monthly Tasks")
-                        .underline()
-                        .font(.title3)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.blue)
-                        .textCase(.uppercase)
+                    // MARK: Nightly Tasks
+                    Group {
+                        Divider()
+                        HStack{
+                            Text(Image(systemName: "moon.stars"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Nightly Tasks")
+                                .underline()
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.yellow)
+                                .textCase(.uppercase)
+                            
+                        }
+                        
+                       
+                        Text("Check all windows")
+                        Text("Check all doors")
+                        Text("Check that the safe is locked")
+                        Text("Check the mailbox")
+                        Text("Inspect security Camares")
+                        Text("Clear ice from sidewalks")
+                        Text("Document \"strange and unusual\" ocurrenses")
+                    }
+                    
+                    // MARK: Weekly Tasks
+                    Group{
+                        Divider()
+                        HStack{
+                            
+                            Text(Image(systemName: "sunset"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Weekly Tasks")
+                                .underline()
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.yellow)
+                                .textCase(.uppercase)
+                           
+                        }
+                        Text("Check inside all vacant rooms")
+                        Text("Walk the perimeter of porperty")
+                    }
+                    
+                    // MARK: Monthly Tasks
+                    Group{
+                        Divider()
+                        HStack{
+                           Text( Image(systemName: "calendar"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Monthly Tasks")
+                                .underline()
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.yellow)
+                                .textCase(.uppercase)
+                        }
+                        Text("Test security alarma")
+                        Text("Test motion detectors")
+                        Text("Test smoke alarms")
+                    }
+                
+                    
                 }.foregroundColor(.gray)
                 Spacer()
             }
