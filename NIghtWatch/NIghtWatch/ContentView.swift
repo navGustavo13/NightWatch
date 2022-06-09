cbb83e1e372f6c10accde7871d8b50d6530abb3e
Gustavo Salazar <gustavo.salazar@accenture.com>
2022-06-09 13:59:08 -0500

    Creating data driven list
    Se uso el List y el foreach para crear un listado de textos

--- NIghtWatch/NIghtWatch/ContentView.swift
+++ NIghtWatch/NIghtWatch/ContentView.swift
@@ -7,90 +7,68 @@
 
 import SwiftUI
 
+let nightlyTasks = [
+            "Check all windows",
+            "Check all doors",
+            "Check that the safe is locked",
+            "Check the mailbox",
+            "Inspect security cameras",
+            "Clear ice from sidewalks",
+            "Document \" strange and ususual \" ocurrences"
+]
+
+
+let weeklyTasks = [
+        "Check inside all vacant rooms",
+        "Walk the perimeter of property"
+]
+
+let monthlyTasks = [
+        "Test security alarm",
+        "Test motion detectors",
+        "Test smoke alarms"
+]
+
 struct ContentView: View {
     var body: some View {
         
-        VStack{
-            HStack {
-                VStack(alignment: .leading){
-                    // MARK: Nightly Tasks
-                    Group {
-                        Divider()
+      
+        List {
+            
+            Section(header:
                         HStack{
-                            Text(Image(systemName: "moon.stars"))
-                                .foregroundColor(.yellow)
-                                .font(.title3)
-                                .fontWeight(.heavy)
+                            Image(systemName: "sunset")
                             Text("Nightly Tasks")
-                                .underline()
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                                .foregroundColor(.yellow)
-                                .textCase(.uppercase)
-                            
-                        }
-                        
-                       
-                        Text("Check all windows")
-                        Text("Check all doors")
-                        Text("Check that the safe is locked")
-                        Text("Check the mailbox")
-                        Text("Inspect security Camares")
-                        Text("Clear ice from sidewalks")
-                        Text("Document \"strange and unusual\" ocurrenses")
-                    }
-                    
-                    // MARK: Weekly Tasks
-                    Group{
-                        Divider()
-                        HStack{
-                            
-                            Text(Image(systemName: "sunset"))
-                                .foregroundColor(.yellow)
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                            Text("Weekly Tasks")
-                                .underline()
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                                .foregroundColor(.yellow)
-                                .textCase(.uppercase)
-                           
-                        }
-                        Text("Check inside all vacant rooms")
-                        Text("Walk the perimeter of porperty")
-                    }
-                    
-                    // MARK: Monthly Tasks
-                    Group{
-                        Divider()
-                        HStack{
-                           Text( Image(systemName: "calendar"))
-                                .foregroundColor(.yellow)
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                            Text("Monthly Tasks")
-                                .underline()
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                                .foregroundColor(.yellow)
-                                .textCase(.uppercase)
-                        }
-                        Text("Test security alarma")
-                        Text("Test motion detectors")
-                        Text("Test smoke alarms")
-                    }
+                                
+            }.font(.title3)) {
+                ForEach(nightlyTasks,id: \.self , content:{
+                            taskName in
+                            Text(taskName)
+                    })
                 
-                    
-                }.foregroundColor(.gray)
-                Spacer()
             }
-            .padding(.all)
-            Spacer()
-        }
-        
-       
-        
+            
+            Section(header:HStack{
+                Image(systemName: "calendar")
+                Text("Montly Tasks")
+            }.font(.title3)) {
+                ForEach(weeklyTasks,id: \.self , content:{
+                            taskName in
+                            Text(taskName)
+                    })
+                
+            }
+            
+            Section {
+                
+                ForEach(monthlyTasks,id: \.self , content:{
+                            taskName in
+                            Text(taskName)
+                    })
+            }
+            
+           
+        }.listStyle(GroupedListStyle())
     }
 }
 
@@ -99,3 +77,89 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+/*
+ 
+ VStack{
+     HStack {
+         VStack(alignment: .leading){
+             // MARK: Nightly Tasks
+             Group {
+                 Divider()
+                 HStack{
+                     Text(Image(systemName: "moon.stars"))
+                         .foregroundColor(.yellow)
+                         .font(.title3)
+                         .fontWeight(.heavy)
+                     Text("Nightly Tasks")
+                         .underline()
+                         .font(.title3)
+                         .fontWeight(.heavy)
+                         .foregroundColor(.yellow)
+                         .textCase(.uppercase)
+                     
+                 }
+                 
+                
+                 Text("Check all windows")
+                 Text("Check all doors")
+                 Text("Check that the safe is locked")
+                 Text("Check the mailbox")
+                 Text("Inspect security Camares")
+                 Text("Clear ice from sidewalks")
+                 Text("Document \"strange and unusual\" ocurrenses")
+             }
+             
+             // MARK: Weekly Tasks
+             Group{
+                 Divider()
+                 HStack{
+                     
+                     Text(Image(systemName: "sunset"))
+                         .foregroundColor(.yellow)
+                         .font(.title3)
+                         .fontWeight(.heavy)
+                     Text("Weekly Tasks")
+                         .underline()
+                         .font(.title3)
+                         .fontWeight(.heavy)
+                         .foregroundColor(.yellow)
+                         .textCase(.uppercase)
+                    
+                 }
+                 Text("Check inside all vacant rooms")
+                 Text("Walk the perimeter of porperty")
+             }
+             
+             // MARK: Monthly Tasks
+             Group{
+                 Divider()
+                 HStack{
+                    Text( Image(systemName: "calendar"))
+                         .foregroundColor(.yellow)
+                         .font(.title3)
+                         .fontWeight(.heavy)
+                     Text("Monthly Tasks")
+                         .underline()
+                         .font(.title3)
+                         .fontWeight(.heavy)
+                         .foregroundColor(.yellow)
+                         .textCase(.uppercase)
+                 }
+                 Text("Test security alarma")
+                 Text("Test motion detectors")
+                 Text("Test smoke alarms")
+             }
+         
+             
+         }.foregroundColor(.gray)
+         Spacer()
+     }
+     .padding(.all)
+     Spacer()
+ }
+ 
+
+ 
+ 
+ */
